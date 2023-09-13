6e78f56923b8be2657ace1f8e020f1665647ec23
EMERSON AVILA <106774143+EmersonAvilaDev@users.noreply.github.com>
2023-09-13 00:15:53 -0300

    Update authDoc.js

--- src/middlewares/authDoc.js
+++ src/middlewares/authDoc.js
@@ -2,32 +2,40 @@ const authDocProducao = async (req, res, next) => {
   const { senha } = req.body;
 
   if (req.headers.host.includes("localhost") || req.originalUrl !== "/doc/") {
+    //Usuário está no localhost
     return next();
   }
 
   if (senha === process.env.SWAGGER_SENHA_DOC) {
+    //Usuário Digitou a senha corretamente
     return next();
   }
 
-  if(senha) {
+  if (senha) {
+    //Usuário Digitou a senha incorretamente
     res.status(401).set("Content-Type", "text/html");
-    res.send(Buffer.from(`
+    res.send(
+      Buffer.from(`
         <form method="post">
             <p style="color: red;">Senha Errada!</p>
             <label for="senha"> Senha da Documentação: </label> 
-            <input type="password" name="senha" id="name"
+            <input type="password" name="senha" id="name" />
             <button type="submit">Entrar</button>
         </form>  
-    `))
-  }else {
+    `)
+    );
+  } else {
+    //Usuário ainda não digitou a senha e está em modo produção
     res.status(200).set("Content-Type", "text/html");
-    res.send(Buffer.from(`
+    res.send(
+      Buffer.from(`
         <form method="post">
             <label for="senha"> Senha da Documentação: </label> 
-            <input type="password" name="senha" id="name"
+            <input type="password" name="senha" id="name" />
             <button type="submit">Entrar</button>
         </form>  
-    `))
+    `)
+    );
   }
 };
 
