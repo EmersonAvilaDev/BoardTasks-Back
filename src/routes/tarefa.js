2002610e836c39dcd5591e4f84a04f9e29cb1f70
EMERSON AVILA <106774143+EmersonAvilaDev@users.noreply.github.com>
2023-09-15 12:39:35 -0300

    Edição de tarefa

--- src/routes/tarefa.js
+++ src/routes/tarefa.js
@@ -30,4 +30,44 @@ router.post("/criar", authUser, connectDataBase, async function (req, res) {
   }
 });
 
+router.put("/editar/:id", authUser, connectDataBase, async function (req, res) {
+  try {
+    // #swagger.tags = ['Tarefa']
+    let idTarefa = req.params.id;
+    let { posicao, titulo, descricao, status, dataEntrega } = req.body;
+    const usuarioLogado = req.usuarioJwt.id;
+    const checkTarefa = await taskSchema.findOne({
+      _id: idTarefa,
+      usuarioCriador: usuarioLogado,
+    });
+    if (!checkTarefa) {
+      throw new Error("Tarefa não encontrada ou pertence a outro usuário");
+    }
+
+    const tarefaAtualizada = await taskSchema.updateOne(
+      { _id: idTarefa },
+      {
+        posicao,
+        titulo,
+        descricao,
+        status,
+        dataEntrega,
+      }
+    );
+    if (tarefaAtualizada?.modifiedCount > 0) {
+      const dadosTarefa = await taskSchema
+        .findOne({ _id: idTarefa })
+        .populate("usuarioCriador");
+
+      res.status(200).json({
+        status: "OK",
+        statusMessage: "Tarefa atualizada com sucesso.",
+        response: dadosTarefa,
+      });
+    }
+  } catch (error) {
+    return handleExpectedErros(res, error);
+  }
+});
+
 module.exports = router;
